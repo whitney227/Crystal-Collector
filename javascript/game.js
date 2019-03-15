24aa46396cb916676640ec5d13cb569de30dbb25
Whitney Crawford <whitneylcrawford@gmail.com>
2019-03-15 18:20:28 -0400

    added HTML and CSS style

---
+++ javascript/game.js
@@ -0,0 +1,5 @@
+    $(document).ready(function() {
+    
+    //Global Variables
+    crystals = ['images/blue-gem.png', 'images/orange-gem.png', 'images/pink-gem.png','images/yellow-gem.png']
+    });
